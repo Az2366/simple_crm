461255fb76fefb2ea18e4edc906c1426b7a0e797
az2366 <az.wong2366@gmail.com>
2024-11-24 17:27:33 +0800

    Done

--- src/main/java/sg/edu/ntu/simple_crm/assignment/Product.java
+++ src/main/java/sg/edu/ntu/simple_crm/assignment/Product.java
@@ -3,6 +3,8 @@ package sg.edu.ntu.simple_crm.assignment;
 public class Product {
     private String id;
     private String name;
+    private double price;
+    private String desc;
 
     public Product() {
     }
@@ -10,6 +12,13 @@ public class Product {
     public Product(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Product(String id, String name, double price, String desc) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
     }
 
     public String getId() {
@@ -26,6 +35,22 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
     }
 
 }
--- src/main/java/sg/edu/ntu/simple_crm/assignment/ProductController.java
+++ src/main/java/sg/edu/ntu/simple_crm/assignment/ProductController.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,19 @@ public class ProductController {
     private ArrayList<Product> products = new ArrayList<>();
 
     public ProductController() {
-        products.add(new Product("1", "Apple"));
-        products.add(new Product("2", "Banana"));
-        products.add(new Product("3", "Carrot"));
+        // products.add(new Product("1", "Apple"));
+        // products.add(new Product("2", "Banana"));
+        // products.add(new Product("3", "Carrot"));
+        products.add(new Product("1", "Apple", 3, "From UK"));
+        products.add(new Product("2", "Banana", 2, "From Thailand"));
+        products.add(new Product("3", "Carrot", 1, "Local"));
+    }
+
+    // Add Product
+    @PostMapping("")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        products.add(product);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
     // Get All Product
@@ -32,6 +45,18 @@ public class ProductController {
         try {
             int index = getProductIndex(id);
             return new ResponseEntity<>(products.get(index), (HttpStatus.OK));
+        } catch (ProductNotFound e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable String id) {
+        try {
+            int index = getProductIndex(id);
+            products.remove(index);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (ProductNotFound e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
